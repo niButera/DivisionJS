fff697a3bc4e016a96b67e992e5fc3afb70f554e
niButera <nikolasxp683@gmail.com>
2023-11-19 21:22:08 -0300

    Arumei tudo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var numerador = prompt("Escolha um numerador:");
-var denominador = prompt("Agora escolha um denominador");
+function realizarDivisao() {
+    var numerador = document.getElementById("numerador").value;
+    var denominador = document.getElementById("denominador").value;
 
-function realizarDivisao(numerador, denominador) {
     if (denominador !== "0") {
         var resultado = numerador / denominador;
         window.alert(`Resultado da divisão: ${resultado}`);
@@ -9,9 +9,5 @@ function realizarDivisao(numerador, denominador) {
         window.alert("Não é possível dividir por zero.");
     }
 
-    // Recarrega a página ao clicar em OK
     window.location.reload();
 }
-
-realizarDivisao(numerador, denominador);
-
